361cb45b4b067c808491aef3b37ce5ba780dd957
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-14 23:10:59 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1
+n = int(input())
+print(sum(range(1, n+1)))
+
+# 2
+n = int(input())
+print(n % 2 == 0)
+
+# 3
+n = int(input())
+print(len(str(n)))
+
+# 4
+a, b = map(int, input().split())
+print(max(a, b))
+
+# 5
+n = int(input())
+print(sum(map(int, str(n))))
